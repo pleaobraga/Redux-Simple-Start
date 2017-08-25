4cccf1579dd9489dbe864f2fcd47c6b03d335f03
pleaobraga <pleao.braga@gmail.com>
2017-08-25 09:45:14 -0300

    start to create a video list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_Key = "AIzaSyAn-WgYVoZr3F2sYWBh1JLlLincfaxFRkk";
 
-const App = () => {
-    return  (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_Key, term: 'stufboards'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return  (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }   
 }
 
 // class App extends Component{
