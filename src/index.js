4b8e25441c34cc99bc0b61b2ed638119de853bea
pedro <pedro.braga@maxmilhas.com.br>
2017-08-25 13:26:11 -0300

    created video list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_Key = "AIzaSyAn-WgYVoZr3F2sYWBh1JLlLincfaxFRkk";
 
@@ -10,10 +11,16 @@ class App extends Component {
     constructor(props){
         super(props)
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null 
+        };
 
         YTSearch({key: API_Key, term: 'stufboards'}, (videos) => {
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -21,16 +28,13 @@ class App extends Component {
         return  (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList  
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }   
 }
-
-// class App extends Component{
-//     render(){
-//         return <p>Xablauy</p>
-//     }
-// }
 
 ReactDom.render(<App />, document.querySelector('.container'));
